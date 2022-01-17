c7c74f1490585e0e77c3e14ae604abe33581fe30
theTejMahal <tejalpatwardhan@gmail.com>
2022-01-17 14:49:19 -0500

    Time activation instead of click
    Time activation instead of click

--- popup.js
+++ popup.js
@@ -5,8 +5,20 @@ chrome.storage.sync.get("color", ({ color}) => {
 	changeColor.style.backgroundColor = color;
 });
 
+// Every 1000 seconds do something
+var time = 1000
+setInterval(async () => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+	
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: setPageBackgroundColor,
+	});
+}, time);
+
 
 // When the button is clicked, inject setPageBackgroundColor into current page
+
 changeColor.addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
 	
